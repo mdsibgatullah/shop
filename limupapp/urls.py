7ead19b923773f09f6081c335df92a4c9bd2d279
munna <msmunnainfo@gmail.com>
2024-08-07 21:30:32 +0600

    url added

--- limupapp/urls.py
+++ limupapp/urls.py
@@ -1,22 +1,21 @@
 # from django.urls import path
 # from limupapp.views import AboutUs, BlogDetailsLeftSidebar, Error, Index, ShopLeftSidebar
 
-# urlpatterns = [
-#     path('',Index.as_view(), name='index'),
-#     path('error/',Error.as_view(), name='error'),
-#     path('about-us/',AboutUs.as_view(), name='about-us'),
-#     path('shop-left-sidebar/',ShopLeftSidebar.as_view(), name='shop-left-sidebar'),
-#     path('blog-details-left-sidebar/',BlogDetailsLeftSidebar.as_view(), name='blog-details-left-sidebar'),
-# ]
-
 from django.urls import path
-from limupapp.views import AboutUs, BlogDetailsLeftSidebar, Error, Index, ShopLeftSidebar, SingleProduct
+from . import views
 
 urlpatterns = [
-    path('',Index.as_view(), name='index'),
-    path('error/',Error.as_view(), name='error'),
-    path('about-us/',AboutUs.as_view(), name='about-us'),
-    path('shop/',ShopLeftSidebar.as_view(), name='shop'),
-    path('single-product/<slug>',SingleProduct.as_view(), name='single-product'),
-    path('blog-details-left-sidebar/',BlogDetailsLeftSidebar.as_view(), name='blog-details-left-sidebar'),
+    path('',views.Index.as_view(), name='index'),
+    path('error/',views.Error.as_view(), name='error'),
+    path('about-us/',views.AboutUs.as_view(), name='about-us'),
+    path('shop/',views.ShopLeftSidebar.as_view(), name='shop'),
+    path('single-product/<slug>',views.SingleProduct.as_view(), name='single-product'), #off link
+    path('blog-details-left-sidebar/',views.BlogDetailsLeftSidebar.as_view(), name='blog-details-left-sidebar'),
+    path('blog-left-sidebar/',views.BlogLeftSidebar.as_view(), name='blog-left-sidebar'),
+    path('checkout/',views.Checkout.as_view(), name='checkout'),
+    path('compare/',views.Compare.as_view(), name='compare'),
+    path('contact/',views.Contact.as_view(), name='contact'),
+    path('faq/',views.Faq.as_view(), name='faq'),
+    path('login/',views.loginRegister.as_view(), name='login'),
+    path('wishlist/',views.Wishlist.as_view(), name='wishlist'),
 ]
